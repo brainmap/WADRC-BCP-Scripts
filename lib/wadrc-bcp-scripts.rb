ef00cce184c89f22ee5846774cb01b806c23df66
Erik <ekk@medicine.wisc.edu>
2011-08-30 16:26:55 -0500

    Update to new bundler gem Version style

--- lib/wadrc-bcp-scripts.rb
+++ lib/wadrc-bcp-scripts.rb
@@ -6,9 +6,8 @@ require 'wadrc-bcp-scripts/basic_task'
 require 'wadrc-bcp-scripts/dtitask'
 require 'wadrc-bcp-scripts/fieldmap_classes'
 require 'wadrc-bcp-scripts/tensor'
+require 'wadrc-bcp-scripts/version'
 require 'additions/NetSshConnectionSession'
 
 # WADRC Basic Common Processing (BCP) Scripts
-module WadrcBcpScripts
-  VERSION = '0.0.5'
-end
+module WadrcBcpScripts; end
